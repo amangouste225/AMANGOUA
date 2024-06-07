02e45884a6946ef424d97920b8f60e9e4ee650a9
Amangoua Kacou Ange-Thierry <104840429+amangouste225@users.noreply.github.com>
2024-06-07 01:29:43 +0000

    layout

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,21 @@
+import { useState } from "react";
 import "../styles/index.css";
 
 import { AiOutlineMinus } from "react-icons/ai";
 import { FaRegSquareFull } from "react-icons/fa6";
 import { RxCross1 } from "react-icons/rx";
+import { HiOutlineSquare2Stack } from "react-icons/hi2";
 
 export default function Sidebar() {
+  const [add, setAdd] = useState(false);
+
+  console.log(add);
   return (
     <div className="logo_sidebar w-full relative">
       <div className="flex justify-between items-center h-full">
-        <div className="w-4 flex-center text-white">A</div>
+        <div className="w-4 flex-center text-white text-xl">
+          <a href="/">A</a>
+        </div>
         <div className="text-white font-normal text-lg font-PPNeue abs-center">
           <a href="/">
             <h4>
@@ -17,11 +24,15 @@ export default function Sidebar() {
           </a>
         </div>
         <div className="flex-center gap-3 p-2">
-          <a href="/" onclick="">
+          <a href="/">
             <AiOutlineMinus size={13} color="gray" />
           </a>
-          <a href="#">
-            <FaRegSquareFull size={13} color="gray" />
+          <a href="#" onChange={() => setAdd((prev) => !prev)}>
+            {add ? (
+              <HiOutlineSquare2Stack size={13} color="gray" />
+            ) : (
+              <FaRegSquareFull size={13} color="gray" />
+            )}
           </a>
           <a href="#">
             <RxCross1 size={13} color="gray" />
